f1a0581cbadbb0dc735764080cf93bf0378c5471
Aurorayqz <aurorayqz@foxmail.com>
2017-10-28 10:36:40 +0800

    ok
    fine

---
+++ app/src/main/java/aurorayqz/packagecom/myapplication/ui/cnmusic/MainActivity.java
@@ -0,0 +1,17 @@
+package aurorayqz.packagecom.myapplication.ui.cnmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import aurorayqz.packagecom.myapplication.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
