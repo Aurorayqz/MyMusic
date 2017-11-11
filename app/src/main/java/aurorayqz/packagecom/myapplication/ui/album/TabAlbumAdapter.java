c73ab8d7d909b800cd19235af6facdd305101c56
Aurorayqz <aurorayqz@foxmail.com>
2017-11-11 13:53:43 +0800

    11.11_add_TabAlbunAdapter

---
+++ app/src/main/java/aurorayqz/packagecom/myapplication/ui/album/TabAlbumAdapter.java
@@ -0,0 +1,25 @@
+package aurorayqz.packagecom.myapplication.ui.album;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Aurorayqz on 2017/11/10.
+ */
+
+public class TabAlbumAdapter extends FragmentPagerAdapter{
+    public TabAlbumAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
